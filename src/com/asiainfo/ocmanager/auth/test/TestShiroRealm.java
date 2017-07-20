a46df06bdefd1d81ba2e0211dc03934885994f4a
qianguo1 <guoqian3@asiainfo.com>
2017-07-20 18:26:38 +0800

    fix bug of testShiroRealm

--- src/com/asiainfo/ocmanager/auth/test/TestShiroRealm.java
+++ src/com/asiainfo/ocmanager/auth/test/TestShiroRealm.java
@@ -18,24 +18,26 @@ public class TestShiroRealm {
 
     @Test
     public void testIniRealm() {
+        try {
         //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("shiro.ini");
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("shiro.inig");
         //2、得到SecurityManager实例 并绑定给SecurityUtils
         org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
         SecurityUtils.setSecurityManager(securityManager);
         //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
-        try {
-            //4、登录，即身份验证
-            subject.login(token);
-            System.out.println(token.toString());
-        } catch (AuthenticationException e) {
-            //5、身份验证失败
-        }
+        //4、登录，即身份验证
+        subject.login(token);
+        System.out.println(token.toString());
         Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
         //6、退出
         subject.logout();
+        } catch (Exception e) {
+            //5、身份验证失败
+            System.out.println(e.getMessage());
+        }
+
     }
 
     @Test
