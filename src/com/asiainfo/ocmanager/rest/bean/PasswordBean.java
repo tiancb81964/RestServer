4b7c12e3ff2040d82244513199bb7bf7c5af7d77
zhaoyim <yyyiming@163.com>
2017-08-01 15:38:25 +0800

    add password bean

---
+++ src/com/asiainfo/ocmanager/rest/bean/PasswordBean.java
@@ -0,0 +1,24 @@
+package com.asiainfo.ocmanager.rest.bean;
+
+/**
+ * 
+ * @author zhaoyim
+ *
+ */
+public class PasswordBean {
+
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "PasswordBean [password: " + password + "]";
+	}
+}
