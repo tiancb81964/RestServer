0c0f3088fa56f572c98dc0c044297211a5f71bc8
qianguo1 <guoqian3@asiainfo.com>
2017-08-04 10:55:49 +0800

    Authentication change from Jdbc to Ldap

--- src/com/asiainfo/ocmanager/auth/test/TestShiroRealm.java
+++ src/com/asiainfo/ocmanager/auth/test/TestShiroRealm.java
@@ -17,50 +17,39 @@ import org.junit.Test;
 public class TestShiroRealm {
 
     @Test
-    public void testIniRealm() {
+    public void testLdapRealm() {
         try {
-        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("shiro.ini");
-        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("shiroLdap.ini");
         org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
         SecurityUtils.setSecurityManager(securityManager);
-        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
         Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
-        //4、登录，即身份验证
+        UsernamePasswordToken token = new UsernamePasswordToken("gq", "123");
         subject.login(token);
         System.out.println(token.toString());
-        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
-        //6、退出
+        Assert.assertEquals(true, subject.isAuthenticated());
         subject.logout();
         } catch (Exception e) {
-            //5、身份验证失败
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
 
     }
 
     @Test
     public void testJdbcRealm() {
-        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
         Factory<SecurityManager> factory = new IniSecurityManagerFactory("shiroJdbc.ini");
-        //2、得到SecurityManager实例 并绑定给SecurityUtils
         org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
         SecurityUtils.setSecurityManager(securityManager);
-        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken("u1", "passw0rd");
         try {
-            //4、登录，即身份验证
             subject.login(token);
             System.out.println("login success!");
             System.out.println(token.toString());
         } catch (AuthenticationException e) {
             System.out.println(e.getMessage());
-            //5、身份验证失败
         }
-        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
-        //6、退出
+        Assert.assertEquals(true, subject.isAuthenticated());
         subject.logout();
     }
 
