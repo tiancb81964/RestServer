950f8bf5f150871efe5f924c8174be889138d36c
EthanWang <autumn1224@163.com>
2017-08-14 16:59:34 +0800

    Filter log enhancement

--- src/com/asiainfo/ocmanager/rest/filter/AuthFilter.java
+++ src/com/asiainfo/ocmanager/rest/filter/AuthFilter.java
@@ -20,50 +20,49 @@ import com.asiainfo.ocmanager.rest.constant.ResponseCodeConstant;
  * Created by gq on 17/7/18.
  */
 public class AuthFilter implements Filter {
-    private static Logger logger = Logger.getLogger(AuthFilter.class);
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+	private static Logger logger = Logger.getLogger(AuthFilter.class);
 
-    }
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
-        HttpServletRequest hsRequest = (HttpServletRequest) servletRequest;
-        logger.info("Request URI: "+hsRequest.getRequestURI() + ", begin servlet filter......");
-        if (hsRequest.getRequestURI().endsWith("/login")) {
-            filterChain.doFilter(servletRequest,servletResponse);
-        } else {
-            try {
-                String token = hsRequest.getHeader("token");
-                if(token == null)
-                {
-                    ((HttpServletResponse)servletResponse).sendError(ResponseCodeConstant.FORBIDDEN);
-                }else {
-                    boolean authcSuccess = authenticate(token);
-                    if(authcSuccess) {
-                        logger.info("Authentication success with token: " + token);
-                        filterChain.doFilter(servletRequest,servletResponse);
-                    }
-                    else {
-                        logger.warn("Authentication fail with token: " + token);
-                        ((HttpServletResponse)servletResponse).sendError(ResponseCodeConstant.FORBIDDEN);
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                ((HttpServletResponse)servletResponse).sendError(ResponseCodeConstant.FORBIDDEN);
-            }
-        }
-    }
+	}
 
-    @Override
-    public void destroy() {
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+			throws IOException, ServletException {
+		HttpServletRequest hsRequest = (HttpServletRequest) servletRequest;
+		logger.debug("Request received: " + hsRequest.getRequestURI());
+		if (hsRequest.getRequestURI().endsWith("/login")) {
+			filterChain.doFilter(servletRequest, servletResponse);
+		} else {
+			try {
+				String token = hsRequest.getHeader("token");
+				if (token == null) {
+					((HttpServletResponse) servletResponse).sendError(ResponseCodeConstant.FORBIDDEN);
+				} else {
+					boolean authcSuccess = authenticate(token);
+					if (authcSuccess) {
+						logger.debug("Authentication success with token: " + token);
+						filterChain.doFilter(servletRequest, servletResponse);
+					} else {
+						logger.warn("Authentication fail with token: " + token);
+						((HttpServletResponse) servletResponse).sendError(ResponseCodeConstant.FORBIDDEN);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				((HttpServletResponse) servletResponse).sendError(ResponseCodeConstant.FORBIDDEN);
+			}
+		}
+	}
 
-    }
+	@Override
+	public void destroy() {
 
-    public boolean authenticate(String token) {
-        return Authenticator.loginWithToken(token);
-    }
+	}
+
+	public boolean authenticate(String token) {
+		return Authenticator.loginWithToken(token);
+	}
 
 }
