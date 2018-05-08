ab9593de1147f0849473a61ba329ec531dfaec5b
wangchen5 <wangchen5@asiainfo.com>
2018-05-08 12:02:10 +0800

    PRDBD_BUG_20180503_0015 fix tenant quota can not be updated after new services been added

---
+++ src/com/asiainfo/ocmanager/utils/QuotaParser.java
@@ -0,0 +1,23 @@
+package com.asiainfo.ocmanager.utils;
+
+import java.util.Map;
+
+import com.google.common.collect.Multimap;
+
+/**
+ * To parse quota string
+ * @author Ethan
+ *
+ */
+public class QuotaParser {
+
+	public static Multimap<String, String> parse(String quotaString){
+		//TODO:
+		return null;
+	}
+	
+	public static String toString(Multimap<String, String> quotas) {
+		//TODO:
+		return null;
+	}
+}
