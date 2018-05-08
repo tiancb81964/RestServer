65ce0ec0e63e8b529047a9896248589ba45a07ae
wangchen5 <wangchen5@asiainfo.com>
2018-05-08 15:09:48 +0800

    PRDBD_BUG_20180503_0015           fix tenant quota can not be updated after new services been 	  added

--- src/com/asiainfo/ocmanager/utils/QuotaParser.java
+++ src/com/asiainfo/ocmanager/utils/QuotaParser.java
@@ -1,8 +1,13 @@
 package com.asiainfo.ocmanager.utils;
 
-import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
-import com.google.common.collect.Multimap;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * To parse quota string
@@ -11,13 +16,29 @@ import com.google.common.collect.Multimap;
  */
 public class QuotaParser {
 
-	public static Multimap<String, String> parse(String quotaString){
-		//TODO:
-		return null;
+	public static Table<String, String, Number> parse(String quotaString){
+		Table<String, String, Number> table = HashBasedTable.create();
+		JsonElement quota = new JsonParser().parse(quotaString);
+		Set<Entry<String, JsonElement>> quotas = quota.getAsJsonObject().entrySet();
+		quotas.forEach(e -> {
+			Set<Entry<String, JsonElement>> keys = e.getValue().getAsJsonObject().entrySet();
+			keys.forEach(k -> {
+				table.put(e.getKey(), k.getKey(), k.getValue().getAsNumber());
+			});
+		});
+		return table;
 	}
 	
-	public static String toString(Multimap<String, String> quotas) {
-		//TODO:
-		return null;
+	public static String toString(Table<String, String, Number> quotas) {
+		JsonObject out = new JsonObject();
+		quotas.rowMap().entrySet().forEach(e -> {
+			JsonObject jsonobj = new JsonObject();
+			e.getValue().forEach((k,v) -> {
+				jsonobj.addProperty(k, v);
+			});
+			out.add(e.getKey(), jsonobj);
+		});
+		return out.toString();
 	}
+	
 }
