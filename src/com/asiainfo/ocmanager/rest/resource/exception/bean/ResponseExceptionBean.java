0c87f2b3e6b03a254a824dd75a8d03c62b4dbee8
zhaoyim <zhaoym6@asiainfo.com>
2018-06-05 15:01:10 +0800

    add response exception bean

---
+++ src/com/asiainfo/ocmanager/rest/resource/exception/bean/ResponseExceptionBean.java
@@ -0,0 +1,25 @@
+package com.asiainfo.ocmanager.rest.resource.exception.bean;
+
+/**
+ * 
+ * @author zhaoyim
+ *
+ */
+public class ResponseExceptionBean {
+
+	private String exception;
+
+	public String getException() {
+		return exception;
+	}
+
+	public void setException(String exception) {
+		this.exception = exception;
+	}
+
+	@Override
+	public String toString() {
+		return "ResponseExceptionBean: {exception: " + exception + "}";
+	}
+
+}
